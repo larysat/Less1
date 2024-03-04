2e65f69cf85985a5f2b94a7652f8c0d432744bc5
lturysheva <lturysheva@aras.com>
2024-03-04 20:13:18 +0100

    Home Work 12 less

---
+++ src1/HW12_task1.ts
@@ -0,0 +1,23 @@
+interface User {
+  name: string;
+  age: number;
+  occupation?: string;
+  car?: string;
+  children?: number;
+}
+
+const allUsers: User[] = [
+  {
+    name: 'Max Mustermann',
+    age: 25,
+    occupation: 'Chimney sweep',
+    car: 'VW',
+  },
+  {
+    name: 'Kate Müller',
+    age: 23,
+    occupation: 'Astronaut',
+    children: 2,
+  },
+];
+console.log(allUsers);
